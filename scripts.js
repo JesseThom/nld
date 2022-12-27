040fc0cea8668412599f613aac1fefe2e0543a90
Jesse Thommes <jthommes22@gmail.com>
2022-12-26 22:14:38 -0600

    update

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
 
 console.log("connected");
 
+// console.logs scroll location
+
+// addEventListener('load', function () {
+//     addEventListener('scroll', function () {
+//         console.log(document.documentElement.scrollTop);
+//     });
+// });
